ef95f033a4bbe368f2c6e8278e0f1724b4ed1c74
SaraIsabelDR <159865382+SaraIsabelDR@users.noreply.github.com>
2024-05-28 15:33:09 -0500

    App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LoginSignUp from './components/assets/loginsignup/loginsignup.jsx';
+
+function App() {
+  return (
+    <div >
+      <LoginSignUp />
+    </div>
+  );
+}
+
+export default App;
